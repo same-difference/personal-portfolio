7b95f10b688f6b9624be11680ccaa944c2b46810
same-difference <dnmurillo@gmail.com>
2024-02-12 01:52:51 -0500

    polished up the img links in projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -13,11 +13,13 @@ interface Props {
 export default function Project({ children, name, link, pic, tags, wip }: Props) {
     const boop = { scale: 1.07, color: "#b04c5b" };
     const beegBoop = { scale: 0.85, color: "#b04c5b" };
+    const imgboop = { scale: 1.03 };
+    const imgBeegBoop = { scale: 1.07 };
     
     return(
         <div className="project relative h-fit break-inside-avoid-column p-3 mb-6 md:mb-8 rounded-2xl shadow-[#9d545e] shadow-lg">
-            <a href={link} target="_blank"><img className="pb-2 mx-auto" src={pic} /></a>
-            <a href={link} target="_blank"><motion.h2 whileHover={boop} whileFocus={boop} whileTap={beegBoop} className="project tracking-wide font-bold text-center text-lg md:text-xl pb-2">{name}</motion.h2></a>
+            <a href={link} target="_blank"><motion.img whileHover={imgboop} whileFocus={imgboop} whileTap={imgBeegBoop} className="mx-auto rounded-xl" src={pic} /></a>
+            <a href={link} target="_blank"><motion.h2 whileHover={boop} whileFocus={boop} whileTap={beegBoop} className="project tracking-wide font-bold text-center text-lg md:text-xl py-2">{name}</motion.h2></a>
             { wip ? <div className="wip -mx-3 mb-3 text-center py-0.5 tracking-wider bg-clip-border">wip</div> : ""}
             <p className="mx-auto px-2 pb-6 text-justify lg:text-center">
                 {children}

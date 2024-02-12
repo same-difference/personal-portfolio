784065a49d0246ad2676b4c8e8babce6ebe11744
same-difference <dnmurillo@gmail.com>
2024-02-12 00:50:34 -0500

    fixed projects screen on smaller screens

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -2,9 +2,9 @@ import Project from "../Project"
 
 export default function Projects() {
     return(
-        <section className="relative h-screen flex flex-col justify-center items-center" id="projects">
-            <h1 className="hidden md:block projects w-fit text-4xl md:text-6xl tracking-wide self-center place-self-start mb-8 md:mb-10">Projects</h1>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 lg:gap-6 xl:gap-10 items-start w-2/3 2xl:w-1/2">
+        <section className="relative flex flex-col justify-center items-center" id="projects">
+            <h1 className="projects w-fit text-4xl md:text-6xl tracking-wide self-center place-self-start mb-8 md:mb-10">Projects</h1>
+            <div className="mb-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 lg:gap-6 xl:gap-10 items-start w-2/3 2xl:w-1/2">
                 <Project name="HookedIn" link="https://devpost.com/software/hookedin-n537qp" pic="hookedin.jpg" tags="react javascript express.js git" wip={false}>
                     A react app that uses chatGPT to roast the user's LinkedIn profile.
                 </Project>

85d25af5730365990cd66c008532fadc1f01e414
same-difference <dnmurillo@gmail.com>
2024-04-08 23:57:10 -0400

    Added a new project and an updated resume

--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -5,6 +5,9 @@ export default function Projects() {
         <section className="relative min-h-screen flex flex-col justify-center items-center" id="projects">
             <h1 className="projects w-fit text-4xl md:text-6xl tracking-wide self-center pt-8 mb-8 md:mb-10">Projects</h1>
             <div className="mb-20 columns-1 md:columns-2 lg:columns-3 w-2/3 xl:w-1/2 2xl:w-2/5">
+                <Project name="HookedIn" link="https://devpost.com/software/the-lab-hfynjw" pic="the_lab.png" tags="react redux tailwind ink.js" wip={false}>
+                    A choose-your-own-adventure game built to help the player better understand <a className="underline hover:font-semibold focus:font-semibold active:hover:font-semibold text-[#b04c5b]" href="https://en.wikipedia.org/wiki/Spoon_theory" target="_blank">spoon theory</a>.
+                </Project>
                 <Project name="HookedIn" link="https://devpost.com/software/hookedin-n537qp" pic="hookedin.jpg" tags="react javascript express.js git" wip={false}>
                     A react app that uses chatGPT to roast the user's LinkedIn profile.
                 </Project>
@@ -14,14 +17,11 @@ export default function Projects() {
                 <Project name="Personal Portfolio" link="https://github.com/same-difference/personal-portfolio" pic="personal_portfolio.png" tags="react typescript framer" wip={false}>
                     Yours truly, this gorgeous site.
                 </Project>
-                <Project name="The Unwilling Chef" link="" pic="icons8-moon-and-stars-cute-color-96.png" tags="figma" wip={true}>
+                <Project name="The Unwilling Chef" link="https://youtu.be/ei49MkoxAWM?si=33N_Txl4dKXEyaea" pic="icons8-moon-and-stars-cute-color-96.png" tags="figma" wip={true}>
                     A meal-prep app designed solely off user feedback.
                 </Project>
-                <Project name="Tournament Manager" link="" pic="icons8-constellation-cute-color-96.png" tags="" wip={true}>
-                    A react app to keep scores and stuff.
-                </Project>
             </div>
-            <footer className="absolute bottom-1">Icons by <a target="_blank"  href="https://icons8.com">Icons8</a></footer>
+            <footer className="absolute bottom-1 text-center">Made with &lt;3 by Dianneth Murillo - Icons by <a target="_blank"  href="https://icons8.com">Icons8</a></footer>
         </section>
     );
 }

5486d179f271258c0624c63fbfc853e36b6a7e5d
same-difference <dnmurillo@gmail.com>
2024-10-08 14:05:44 -0400

    swapped over to local fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.tsx'
 import './main.css'
+import './fonts/TextMeOne-Regular.ttf'
+import './fonts/Raleway-VariableFont_wght.ttf'
+import './fonts/Raleway-Italic-VariableFont_wght.ttf'
+import './fonts/Exo2-VariableFont_wght.ttf'
+import './fonts/Exo2-Italic-VariableFont_wght.ttf'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

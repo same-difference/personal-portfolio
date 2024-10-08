5b8be166c9ad138d2db8cffce6409c9bf1580b3a
same-difference <dnmurillo@gmail.com>
2024-10-07 21:18:25 -0400

    Added GuitAR to Projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -7,7 +7,7 @@ interface Props {
     link: string;
     pic: string;
     tags: string;
-    wip: boolean;
+    wip: string;
 }
 
 export default function Project({ children, name, link, pic, tags, wip }: Props) {
@@ -20,7 +20,7 @@ export default function Project({ children, name, link, pic, tags, wip }: Props)
         <div className="project relative h-fit break-inside-avoid-column p-3 mb-6 md:mb-8 rounded-2xl shadow-[#9d545e] shadow-lg">
             <a href={link} target="_blank"><motion.img whileHover={imgboop} whileFocus={imgboop} whileTap={imgBeegBoop} className="mx-auto rounded-xl" src={pic} alt={pic} /></a>
             <a href={link} target="_blank"><motion.h2 whileHover={boop} whileFocus={boop} whileTap={beegBoop} className="project tracking-wide font-bold text-center text-lg md:text-xl py-2">{name}</motion.h2></a>
-            { wip ? <div className="wip -mx-3 mb-3 text-center py-0.5 tracking-wider bg-clip-border">wip</div> : ""}
+            { wip ? <div className="wip -mx-3 mb-3 text-center py-0.5 tracking-wider bg-clip-border">{wip}</div> : ""}
             <p className="mx-auto px-2 pb-6 text-justify lg:text-center">
                 {children}
             </p>
--- src/components/pages/Projects.tsx
+++ src/components/pages/Projects.tsx
@@ -7,22 +7,25 @@ export default function Projects() {
         <section className="relative min-h-screen flex flex-col justify-center items-center" id="projects">
             <h1 className="projects w-fit text-4xl md:text-6xl tracking-wide self-center pt-8 mb-8 md:mb-10">Projects</h1>
             <div className="mb-20 columns-1 md:columns-2 lg:columns-3 w-2/3 xl:w-1/2 2xl:w-2/5">
-                <Project name="Meddy" link="https://devpost.com/software/meddy" pic="meddy.png" tags="react tailwind next.js" wip={false}>
+                <Project name="GuitAR" link="https://devpost.com/software/guitar-o3gw19" pic="guitar.png" tags="unity grokit-core meta-xr-sdk c#" wip="Won 2nd Overall @ KnightHacks 2024">
+                    An AR guitar-teaching app made for the Meta Quest 3.
+                </Project>
+                <Project name="Meddy" link="https://devpost.com/software/meddy" pic="meddy.png" tags="react tailwind next.js" wip="">
                     Meddy provides a centralized database for patient information for patients and healthcare providers alike.
                 </Project>
-                <Project name="The Lab" link="https://devpost.com/software/the-lab-hfynjw" pic="the_lab.png" tags="react redux tailwind ink.js" wip={false}>
+                <Project name="The Lab" link="https://devpost.com/software/the-lab-hfynjw" pic="the_lab.png" tags="react redux tailwind ink.js" wip="">
                     A choose-your-own-adventure game built to help the player better understand {spoon_theory}.
                 </Project>
-                <Project name="HookedIn" link="https://devpost.com/software/hookedin-n537qp" pic="hookedin.jpg" tags="react javascript express.js git" wip={false}>
+                <Project name="HookedIn" link="https://devpost.com/software/hookedin-n537qp" pic="hookedin.jpg" tags="react javascript express.js git" wip="">
                     A react app that uses chatGPT to roast the user's LinkedIn profile.
                 </Project>
-                <Project name="Book Review Site" link="https://students.gaim.ucf.edu/~di118075/dig3134/assignment06/login.php" pic="book_review_site.png" tags="html css php mySQL" wip={false}>
+                <Project name="Book Review Site" link="https://students.gaim.ucf.edu/~di118075/dig3134/assignment06/login.php" pic="book_review_site.png" tags="html css php mySQL" wip="">
                     A proof-of-concept site to practice mySQL databases.
                 </Project>
-                <Project name="Personal Portfolio" link="https://github.com/same-difference/personal-portfolio" pic="personal_portfolio.png" tags="react typescript framer" wip={false}>
+                <Project name="Personal Portfolio" link="https://github.com/same-difference/personal-portfolio" pic="personal_portfolio.png" tags="react typescript framer" wip="">
                     Yours truly, this gorgeous site.
                 </Project>
-                <Project name="The Unwilling Chef" link="https://www.figma.com/design/tfPhmHlqGncP7OJHNhan78/Prototype?node-id=310-1619" pic="unwilling_chef.png" tags="figma ui/ux" wip={false}>
+                <Project name="The Unwilling Chef" link="https://www.figma.com/design/tfPhmHlqGncP7OJHNhan78/Prototype?node-id=310-1619" pic="unwilling_chef.png" tags="figma ui/ux" wip="">
                     A meal-prep app designed with user-centered design.
                 </Project>
             </div>

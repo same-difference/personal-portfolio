b96d8ce9dd1d8b37437faf11f9064ab1b023c2c3
same-difference <dnmurillo@gmail.com>
2025-04-30 03:19:26 -0400

    changed project card hovering

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -11,20 +11,18 @@ interface Props {
 }
 
 export default function Project({ children, name, link, pic, tags, wip }: Props) {
-    const boop = { scale: 1.07, color: "#b04c5b" };
-    const beegBoop = { scale: 0.85, color: "#b04c5b" };
-    const imgboop = { scale: 1.03 };
-    const imgBeegBoop = { scale: 1.07 };
+    const boop = { scale: 1.03, boxShadow: '0 0 20px 1px #bc626e' };
+    const beegBoop = { scale: 0.94, boxShadow: '0 0 20px 1px #bc626e' };
     
     return(
-        <div className="project relative h-fit break-inside-avoid-column p-3 mb-6 md:mb-8 rounded-2xl shadow-[#9d545e] shadow-lg">
-            <a href={link} target="_blank"><motion.img whileHover={imgboop} whileFocus={imgboop} whileTap={imgBeegBoop} className="mx-auto rounded-xl" src={pic} alt={pic} /></a>
-            <a href={link} target="_blank"><motion.h2 whileHover={boop} whileFocus={boop} whileTap={beegBoop} className="project tracking-wide font-bold text-center text-lg md:text-xl py-2">{name}</motion.h2></a>
+        <motion.a href={link} target="_blank" whileHover={boop} whileFocus={boop} whileTap={beegBoop} className="project block relative h-fit break-inside-avoid-column p-3 mb-6 md:mb-8 rounded-2xl shadow-[#bc626e] shadow-sm">
+            <img className="mx-auto rounded-xl" src={pic} alt={pic} />
+            <h2 className="project tracking-wide font-bold text-center text-lg md:text-xl py-2">{name}</h2>
             { wip ? <div className="wip -mx-3 mb-3 text-center py-0.5 tracking-wider bg-clip-border">{wip}</div> : ""}
             <p className="mx-auto px-2 pb-6 text-justify lg:text-center">
                 {children}
             </p>
             { tags ? <Tags tags={tags}/> : ""}
-        </div>
+        </motion.a>
     );
 }
